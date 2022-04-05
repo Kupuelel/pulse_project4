1dfe11c7939cee9f69b9f76fab39599567f2481e
Kirill <kupuelel@gmail.com>
2022-04-05 16:30:56 +0300

    validateforms

--- src/js/script.js
+++ src/js/script.js
@@ -68,6 +68,22 @@ $(document).ready(function(){
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass(' catalog__content_active').eq($(this).index()).addClass(' catalog__content_active');
   });
+
+  //modal
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn(500);
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #order, #thanks').fadeOut(300);
+  });
+  
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function(){
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
+      $('.overlay, #order').fadeIn(500);
+    });
+  });
 });
 
 //   $('.catalog-item__link').each(function(i) {
@@ -93,6 +109,39 @@ $(document).ready(function(){
         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
       })
     });
+    
+  
+
+    function validateForms(form) {
+      $(form).validate({
+        rules:{
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: {
+            required: true, 
+            email: true
+          }
+        }, 
+        messages: {
+          name: {
+            required: "Пожалуйста введите свое имя",
+            minlength: jQuery.validator.format("Введиет {0} символа!")
+          },
+          phone: "Пожалуйста введите номер телефона",
+          email: {
+            required: "Пожалуйста введите свой email",
+            email: "Неправельно введен email"
+          }
+        }
+    });
+    };
+    validateForms('#consultation-form');
+    validateForms('#order form');
+    validateForms('#consultation form');
+
   };
 
   
